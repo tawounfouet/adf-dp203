87acbbc5b0775ce260ad29acc605c397d2ea9612
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2025-03-04 04:00:01 +0100

    add data & code

---
+++ draft/4. Lab - Processing JSON objects/04.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Customercourse
+(
+    CustomerID int,
+    CustomerName varchar(200),
+    Registered BIT,
+    Courses varchar(200),
+    Mobile varchar(200),
+    City varchar(200)
+)
---
+++ draft/5. Lab - Self-Hosted Runtime - Mapping Data Flow/05.sql
@@ -0,0 +1,9 @@
+CREATE TABLE WebLogs
+(
+    LogDate date,
+    IPAddress varchar(20),
+    RequestMethod varchar(10),
+    RequestResource varchar(1000)
+);
+
+
